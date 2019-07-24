2f4f13c205c422e6e292f0b8d968373bc3d28d12
Joshua-Sifuentes1 <sifuentes.aaronm@gmail.com>
2019-07-24 16:23:53 -0500

    hello world;

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,23 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("hello World!");
+        System.out.println("Hello, World!");
+        int myFavoriteNumber = 473;
+        System.out.println(myFavoriteNumber);
+        String myString = "This is a string!";
+        System.out.println(myString);
+        myString = "This is a new string!";
+        System.out.println(myString);
+        double myNumber;
+        myNumber = 3.14;
+        System.out.println(myNumber);
+        int u = 4;
+        u += 5;
+        int v = 3;
+        v *= 4;
+
+        int x = 10;
+        x /= 2;
+        int y = 2;
+        y -= 10;
     }
 }
