a795badcf21b2df883806b436284977ec1f18b53
Joshua-Sifuentes1 <sifuentes.aaronm@gmail.com>
2019-07-25 14:50:00 -0500

    console exercises

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -2,23 +2,29 @@ import java.util.Scanner;
 public class ConsoleExercises {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-//        double pi = 3.14159;
-//        System.out.format("The value of pi is approximately %.2f\n", pi);
-//        System.out.println("Enter an integer: ");
-//        int userNumber = scanner.nextInt();
-//        System.out.println("You have entered: " + userNumber);
-//        System.out.println("Enter 3 words: ");
-//        String uI = scanner.next();
-//        String uI2 = scanner.next();
-//        String uI3 = scanner.next();
-//        System.out.println("Your 3 words are: ");
-//        System.out.println(uI);
-//        System.out.println(uI2);
-//        System.out.println(uI3);8
-//        System.out.println("Enter a sentence: ");
-//        String userSent = scanner.nextLine();
-//        System.out.println("Your sentence is: ");
-//        System.out.println(userSent);
+        double pi = 3.14159;
+        System.out.format("The value of pi is approximately %.2f\n", pi);
+
+        System.out.println("Enter an integer: ");
+        int userNumber = scanner.nextInt();
+        System.out.println("You have entered: " + userNumber);
+
+        System.out.println("Enter 3 words: ");
+        String uI = scanner.next();
+        String uI2 = scanner.next();
+        String uI3 = scanner.next();
+
+        System.out.println("Your 3 words are: ");
+        System.out.println(uI);
+        System.out.println(uI2);
+        System.out.println(uI3);
+        scanner.nextLine();
+
+        System.out.println("Enter a sentence: ");
+        String userSent = scanner.nextLine();
+        System.out.println("Your sentence is: ");
+        System.out.println(userSent);
+
         System.out.println("Give the height of the classroom: ");
         int height = scanner.nextInt();
         System.out.println("Now give the width of the classroom: ");
