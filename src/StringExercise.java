7966ef7267a840c96b85e41296c3d987dd5d1dac
Joshua-Sifuentes1 <sifuentes.aaronm@gmail.com>
2019-07-29 12:16:02 -0500

    string exercise

--- src/StringExercise.java
+++ src/StringExercise.java
@@ -9,19 +9,20 @@ public class StringExercise {
 //        message += "I can do backslashes \\, double backslashes \\\\, and triple backslashes! \\\\\\\n";
 //        System.out.println(message);
         Scanner scanner = new Scanner(System.in);
+
         System.out.println("A frenetic teen approaches you, emerging from behind the trees" +
                 ", wielding styled hair \nand a polished leather jacket. He seems" +
-                "somewhat distraught, but he approaches you cautiously and slowly.");
+                " somewhat distraught, but he approaches you cautiously and slowly.");
                 int i = 0;
         do {
             System.out.println("What do you say?");
 
-            String question = scanner.nextLine();
+            String question = scanner.nextLine().toLowerCase();
             if (question.contains("who are you")) {
                 System.out.println("\"My name's Bob. For your safety, don't ask questions.\"");
             } else if (question.contains("hi") || question.contains("hello") || question.contains("how's it")) {
                 System.out.println("He waves his hand, looking toward the distance. He's distracted by something.");
-            } else if (question.contains("looking") || question.contains("looking at")) {
+            } else if (question.contains("looking") || question.contains("looking at") || question.contains("what's wrong") || question.contains("whats wrong")) {
                 System.out.println("\"Nothing.\"");
             } else if (question.contains("what are you doing") || question.contains("you doing")) {
                 System.out.println("\"Running. From them.\"");
@@ -34,9 +35,9 @@ public class StringExercise {
             } else if (question.contains("hears us") || question.contains("them")) {
                 System.out.println("\"Them. The creatures. They've been chasing me for -- nevermind.\"");
             } else if(question.contains("creatures") || question.contains("no one here") || question.contains("no one")) {
-                System.out.println("\"I didn't get a good look at them, but they're out there. Watching. Waiting. " +
+                System.out.println("\"I didn't get a good look at them, but they're out there, trust me. Watching. Waiting. " +
                         "Don't walk in the dark alone.\"");
-            } else if (question.contains("bye")) {
+            } else if (question.contains("bye") || question.contains("see ya") || question.contains("later")) {
                 System.out.println("\"I never asked for help anyway. Out of my way.\"");
                 break;
             } else {
