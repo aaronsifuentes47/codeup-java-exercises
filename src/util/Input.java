4c1c50813b003d4fd0514bc5767a113f85fb6ce6
Joshua-Sifuentes1 <sifuentes.aaronm@gmail.com>
2019-08-07 13:24:46 -0500

    exceptions exercise

--- src/util/Input.java
+++ src/util/Input.java
@@ -14,10 +14,15 @@ public class Input {
         String answer = scanner.nextLine();
         return (answer.toLowerCase().contains("yes") || answer.toLowerCase().equalsIgnoreCase("y"));
     }
+    public int getInt(String prompt) {
+        System.out.println(prompt);
+        return this.getInt();
+    }
     public int getInt() {
         int userInput = 0;
         try {
             userInput = Integer.valueOf(scanner.nextLine());
+            System.out.println("You entered: " + userInput);
         } catch (NumberFormatException e) {
             System.out.println("this is an error, please try again: ");
            return getInt();
@@ -30,6 +35,7 @@ public class Input {
         do {
             try {
             answer = Integer.valueOf(scanner.nextLine());
+                System.out.println("You entered: " + answer);
             } catch (NumberFormatException e){
                 System.out.println("BAD NUMBER BAD NUMBER BAD NUMBER");
                 continue;
@@ -55,7 +61,7 @@ public class Input {
 
     public double getDouble(double min, double max) {
         double answer;
-        System.out.printf("Enter a number between %f and %f: ", min, max);
+        System.out.printf("Enter a number between %.2f and %.2f: ", min, max);
         do {
             try {
             answer = Double.valueOf(scanner.nextLine());
@@ -64,9 +70,10 @@ public class Input {
                 continue;
             }
             if (answer >= min && answer <= max) {
+                System.out.println("You entered: " + answer);
                 return answer;
             } else {
-                System.out.printf("Invalid input. Please enter a number between %f and %f.", min, max);
+                System.out.printf("Invalid input. Please enter a number between %.2f and %.2f.", min, max);
             }
         } while (true);
     }
--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -3,7 +3,7 @@ package util;
 public class InputTest {
 
     public static void main(String[] args) {
-        Input string = new Input();
+        Input input = new Input();
 //        do {
 //            string.getString();
 //        } while (string.yesNo());
@@ -11,10 +11,8 @@ public class InputTest {
 //        string.getDouble(15.1, 15.9);
 
         do {
-        System.out.println("enter a number: ");
-            System.out.println("You entered: " + string.getInt(5,20));
-        System.out.println("enter a decimal: ");
-            System.out.println("You entered: " + string.getDouble(5.1,19.9));
-        } while (string.yesNo());
+            input.getInt("Please enter a number: ");
+            input.getDouble(5.1,19.9);
+        } while (input.yesNo());
     }
 }
